e6ca06a9650caf4dd4f2b5688250c6064b5a6c74
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-12-06 23:58:20 -0500

    update to allow vite hmr updates in docker container

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // allows access from outside container
+    port: 5173,
+  },
+});
